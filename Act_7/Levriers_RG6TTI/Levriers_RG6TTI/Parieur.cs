d79050b00809405a1b2c299a625e63e70e100ddd
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2024-03-29 14:44:00 +0100

    pari

--- Act_7/Levriers_RG6TTI/Levriers_RG6TTI/Parieur.cs
+++ Act_7/Levriers_RG6TTI/Levriers_RG6TTI/Parieur.cs
@@ -8,5 +8,36 @@ namespace Levriers_RG6TTI
 {
     internal class Parieur
     {
+        private uint _numChien;
+        private double _longueurPiste;
+        private int _positionCourse;
+        private Image _image;
+        private bool _gagne;
+
+        public uint NumChien
+        {
+            get { return _numChien; }
+            set { _numChien = value; }
+        }
+        public double LongueurPiste
+        {
+            get { return _longueurPiste; }
+            set { _longueurPiste = value; }
+        }
+        public int PositionCourse
+        {
+            get { return _positionCourse; }
+            set { _positionCourse = value; }
+        }
+        public Image Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+        public bool Gagne
+        {
+            get { return _gagne; }
+            set { _gagne = value; }
+        }
     }
 }
